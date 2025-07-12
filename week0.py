cfd275d1acb02940433d5fbd1259c94199fef913
annwong <annwong112761@gmail.com>
2025-07-12 17:24:41 +0800

    add solution to week0.py

--- week0.py
+++ week0.py
@@ -13,6 +13,20 @@
 # HINT: using % operator to find remainder may be helpful
 def dose(needs):
     #YOUR SOLUTION STARTS HERE
+    if (not isinstance(needs, list)):
+        print('Your input should be a list')
+
+    if (sum(needs) <= 0 or sum(needs) >= 500 or (len([i for i in needs if i >= 250]) > 0)):
+        return 'No medicine given'
+    else:
+        out = []
+        for i in needs:
+            vitamins = int(i / 10) if (i % 10 == 0) else int(i / 10 + 1)
+            injections = vitamins * 10 - i
+            print('==', i, vitamins, injections, '==')
+            out.append((vitamins, injections))
+
+        return out
 
     #YOUR SOLUTION ENDS HERE
 
